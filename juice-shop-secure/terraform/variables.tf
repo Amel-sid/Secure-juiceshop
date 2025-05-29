a37ee1c06a528295d9e11b682dfb87f5c9dd4a3c
amel <amel.faths@gmail.com>
2025-05-29 08:15:39 +0200

    feat(terraform): déclare les variables d'entrée nécessaires

--- juice-shop-secure/terraform/variables.tf
+++ juice-shop-secure/terraform/variables.tf
@@ -10,8 +10,20 @@ variable "vm_user" {
   default     = "vagrant"
 }
 
+variable "ssh_key_path" {
+  description = "Chemin vers la clé privée SSH (laisser vide pour détection automatique)"
+  type        = string
+  default     = null
+}
+
 variable "allowed_ports" {
   description = "Ports à ouvrir dans le pare-feu"
   type        = list(number)
   default     = [80, 443, 3000]  # Ports pour Juice Shop + HTTPS/HTTP
+}
+
+variable "inventory_file" {
+  description = "Chemin du fichier d'inventaire Ansible"
+  type        = string
+  default     = "./inventory"
 }
